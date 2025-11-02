339591c8ee114a7d7740f6c91c133748a538bb6d
sjoy825 <tup13001@temple.edu>
2025-11-01 22:04:07 -0400

    Add files via upload

---
+++ preposal.py
@@ -0,0 +1,23 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+Idea 1:
+- A Haunted maze game based on things that are scary for college students
+- Includes: Exam due dates, networking events, group projects, etc. You must
+    find the exit of the game before you are caught by the Final Exam Monster
+
+Idea 2:
+- Tower defense game
+- Includes: People that are scary for college student coming towards the tower
+    like profesors, freshman, dean, political canvasers, advisors, etc.
+    Each person will have a different defense strength that's inflicted if
+    they reach the tower. To stop them, they must be shot away with pens.
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+Shannon Ryan
+
+## Do you have any questions of your own?
+
+Not just yet. Maybe when we start creating the project, I'll have some questions. 
