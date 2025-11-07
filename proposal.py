1eba87e667696349f3422be11f25a760bbd7e097
sjoy825 <tup13001@temple.edu>
2025-11-07 15:25:30 -0500

    Add files via upload

---
+++ proposal.py
@@ -0,0 +1,61 @@
+
+# Proposal
+
+## What will (likely) be the title of your project?
+
+No Final --> College is scary...Finals are deadly
+Degree of Doom --> You're so close to graduating...if you can survive the maze of college life
+The Academic Abyss --> Lost freshman, ghostly advisors, and the Final Exam lurking in the dark
+Nightmare University --> Freshman that want to party, advisors that never answer emails, and semesters that never end
+                
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+A maze that has things that are scary for college students. It will include exam due dates, networking events, group projects, etc. and you have to find the end before you are caught by the Final Exam Monster.
+
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+It will be a 3D POV maze game. We will use either Unreal engine or Unity. The different rooms will have a theme. Some room theme examples include group project, academic advisor pestering you to schedule a
+meeting, college freshman asking where parties are, and a political canvasser asking you for 5 minutes of your time to speak about candidates. The main object of the game is to find the end of the maze
+before the ultimate monster gets you: The Final Exam Monster.
+
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+NA
+
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+Shannon Ryan, shannonryan@temple.edu, Farnoush Nilizadeh
+
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+Definitely a haunted maze that has the Final Exam monster chasing the player.
+
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+Adding the themes and designs and monsters of the freshman and an advisor.
+
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+The hope is that we have time to successfully add both the group project and politcal canvasser monsters.
+
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+First, we need to watch Youtube tutorials on how to design a haunted maze game so we can decide to which software to use. Then, we will need to make sure we understand how to use and navigate the programming
+language. At that point, we can begin to build the basics of the game while following the tutorial. Then, we can begin to add the details that will start with freshman, advisor, group project and political
+canvasser. This will include posters on the wall and college themed items scattered throughout the maze. We also need to make sure that the player will respawn at the beginning of the game if they are
+caught before escaping the maze. If we have additional time to work on our design of the game, we will consider adding additional themes to our maze that may include student debt, deciding a major and/or
+making friends.
+
+Since we are collaborating, we will watch the video tutorials together to agree on which to follow. Then, split the work equally while setting deadlines to make sure we make consistent progress.
+I believe our individual strengths will become apparent as we begin designing the game, in which case our duties will be seperated by what we are good at and what we enjoy.
+
+
