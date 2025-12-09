35ee3ccc926476a36c404e6761558da9964bdb0c
sjoy825 <tup13001@temple.edu>
2025-12-08 21:47:26 -0500

    Add files via upload

---
+++ status.py
@@ -0,0 +1,29 @@
+# Status Report
+
+#### Your name
+
+Shalini Joy
+
+#### Your section leader's name
+
+Negar Koushki (my TA), Shannon Ryan (my project partner) *not sure which one you wanted*
+
+#### Project title
+
+Dune University
+
+***
+
+Short answers for the below questions suffice. If you want to alter your plan for your project (and obtain approval for the same), be sure to email your section leader directly!
+
+#### What have you done for your project so far?
+
+Downloaded Unity and watched a few tutorials on how to operate the program, even created a mini game during the tutorial!
+
+#### What have you not done for your project yet?
+
+Created our maze, figures and themed rooms.
+
+#### What problems, if any, have you encountered?
+
+One program was giving my partner issues and crashing her computer, so we switched to Unity. Unity crashed once for me but I think it had to do with my Mac not the program. 
